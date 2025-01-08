d1c485297d1c8b4649726c92585e7f711c135679
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2025-01-09 00:10:03 +0530

    Update userbot.py

--- nexichat/userbot/userbot.py
+++ nexichat/userbot/userbot.py
@@ -24,10 +24,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_INCRICIBLE")
-                await self.one.join_chat("ABOUT_GODFATHER")
-                await self.one.join_chat("INCRICIBLE")
-                await self.one.join_chat("ZOYU_SUPPORT")
+                await self.one.join_chat("shayariAlfaazonKaAaina")
+                await self.one.join_chat("ABOUT_ur_moonshining")
+                await self.one.join_chat("grandxmasti")
+                await self.one.join_chat("kittyxupdates")
 
             except:
                 pass
