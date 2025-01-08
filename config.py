0160ca92da274b91865f65a1658144bb6e5fcd56
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2025-01-09 00:06:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7520092354"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/legendtosif/CHATBOT")
+OWNER_ID = int(getenv("OWNER_ID", "7297381612"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Moonshining1/CHATBOT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "THE_INCRICIBLE"
-UPDATE_CHNL = "THE_INCRICIBLE"
-OWNER_USERNAME = "LEGEND_MICKEY"
+SUPPORT_GRP = "grandxmasti"
+UPDATE_CHNL = "kittyxupdates"
+OWNER_USERNAME = "moonshining3"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
